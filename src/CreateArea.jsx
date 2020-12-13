e2779951ad76c1b4fe57a2798391633b932b3749
viditk9 <viditkhodiar@gmail.com>
2020-12-13 22:55:26 +0530

    all api calls working

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -109,57 +109,85 @@
 
 //  export default CreateArea;
 import React , {useState,useEffect} from 'react';
+import {Redirect} from 'react-router-dom'
 import './styles.css';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 
 export default function CreateArea() {
   const initialState = {
-            templeName: "BAPS ",
-						address: "Chino Hills",
-						shortDescription: "The BAPS Shri Swaminarayan Mandir[a] is a Hindu temple complex located in Chino Hills, in southwestern San Bernardino County in southern California.",
-						detailedDescription: "The BAPS Shri Swaminarayan Mandir[a] is a Hindu temple complex located in Chino Hills, in southwestern San Bernardino County in southern California. The temple belongs to the Bochasanwasi Akshar Purushottam Swaminarayan Sanstha denomination of Hinduism. However, the BAPS Shri Swaminarayan Mandir is open to visitors of all faiths.",
-						city: "San Bernardino",
-						state: "California",
-						country: "United States",
-						deity: "Shri Swaminarayan",
-						websiteUrl: "https://en.wikipedia.org/wiki/BAPS_Shri_Swaminarayan_Mandir_Chino_Hills",
-						headerImageUrl: "https://en.wikipedia.org/wiki/BAPS_Shri_Swaminarayan_Mandir_Chino_Hills#/media/File:BAPS_Mandir_LA_1.jpg",
-						createdBy: "Aryan"
+            templeName: "",
+						address: "",
+						shortDescription: "",
+						detailedDescription: "",
+						city: "",
+						state: "",
+						country: "",
+						deity: "",
+						websiteUrl: "",
+						headerImageUrl: "",
+						createdBy: "VK"
   }
-  useEffect(() => {
-    axios.post("https://qzsnu26p30.execute-api.us-east-2.amazonaws.com/dev/temples/add", initialState)
+  function handleChange(event) {
+    const { name, value } = event.target;  
+    setTemple(prevNote => {
+      return {
+      ...prevNote,
+      [name]: value
+      };
+    });
+    if(name==="detailedDescription")
+    {
+      setTemple(prevNote => {
+        return {
+        ...prevNote,
+        ["shortDescription"] : value
+        };
+      });
+    }
+  }
+  const [submitted,setsubmit] = useState(false);
+  const [temple,setTemple] = useState(initialState);
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => {
+    console.log(errors);
+    console.log(temple);
+    axios.post("https://qzsnu26p30.execute-api.us-east-2.amazonaws.com/dev/temples/add",temple)
             .then(res => {
-                console.log(res);
-                console.log("Messi");
+                console.log(res.data);
+                setsubmit(true);
             })
             .catch(err => {
                 console.log(err);
-            })
-  }, []);
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
-  
-   return (
+            });
+  }
+  if(submitted) return <Redirect to='/' />
+   
+  return (
     <div>
     <h1 className="adding">Add Temple</h1>
     <form className="create-note" onSubmit={handleSubmit(onSubmit)}>
-      <label><h5>Temple Name</h5><input className="create-note" type="text"  name="Temple_name" ref={register({required: "Temple Name is required", maxLength: {value:80, message:"Name limit 80 characters"}})} /></label>
+      <label><h5>Temple Name</h5><input className="create-note" onChange={handleChange} value={temple.templeName} type="text"  name="templeName" ref={register({required: "Temple Name is required", maxLength: {value:80, message:"Name limit 80 characters"}})} /></label>
       <div>{errors.Temple_name && <span className="error">{errors.Temple_name.message}</span>}</div>
-      <label><h5>Description</h5></label><textarea className="create-note" type="text"  name="Description" ref={register({required: "Description is required", maxLength: {value:1000, message:"Description limit 1000 characters"}})} />
+      <label><h5>Description</h5></label><textarea className="create-note" onChange={handleChange} value={temple.detailedDescription} type="text"  name="detailedDescription" ref={register({required: "Description is required", maxLength: {value:1000, message:"Description limit 1000 characters"}})} />
       <div>{errors.Description && <span className="error">{errors.Description.message}</span>}</div>
-      <label><h5>Image Url</h5></label><input className="create-note" type="url"  name="Image_Url" ref={register({required: "Image url is required"})} />
+      <label><h5>Image Url</h5></label><input className="create-note" onChange={handleChange} value={temple.headerImageUrl} type="url"  name="headerImageUrl" ref={register({required: "Image url is required"})} />
       <div>{errors.Image_Url && <span className="error">{errors.Image_Url.message}</span>}</div>
-      <label><h5>Wiki Link</h5></label><input className="create-note" type="url" name="Wiki Link" ref={register} />
-      <label><h5>Address</h5></label><input className="create-note" type="text" name="Address" ref={register({required: "Address is required"})} />
-      <div>{errors.Address && <span className="error">{errors.Address.message}</span>}</div>
-      <label><h5>Priest Contact Number</h5></label><input className="create-note" type="tel" name="PCN" ref={register({required: "Contact number is required", min: {value:6, message:"Invalid phone number"}, maxLength: {value:12, message:"Invalid phone number"}})} />
+      <label><h5>Wiki Link</h5></label><input className="create-note" onChange={handleChange} value={temple.websiteUrl} type="url" name="websiteUrl" ref={register} />
+      <label><h5>Address</h5></label><input className="create-note" onChange={handleChange} value={temple.address} type="text" name="address" ref={register({required: "Address is required"})} />
+      <div>{errors.address && <span className="error">{errors.address.message}</span>}</div>
+      <label><h5>City</h5></label><input className="create-note" onChange={handleChange} value={temple.city} type="text" name="city" ref={register({required: "Please specify City"})} />
+      <div>{errors.city && <span className="error">{errors.city.message}</span>}</div>
+      <label><h5>State</h5></label><input className="create-note" onChange={handleChange} value={temple.state} type="text" name="state" ref={register({required: "Please specify State"})} />
+      <div>{errors.state && <span className="error">{errors.state.message}</span>}</div>
+      <label><h5>Country</h5></label><input className="create-note" onChange={handleChange} value={temple.country} type="text" name="country" ref={register({required: "Please specify Country"})} />
+      <div>{errors.country && <span className="error">{errors.country.message}</span>}</div>
+      {/* <label><h5>Priest Contact Number</h5></label><input className="create-note" onChange={handleChange} value={temple.templeName} type="tel" name="PCN" ref={register({required: "Contact number is required", min: {value:6, message:"Invalid phone number"}, maxLength: {value:12, message:"Invalid phone number"}})} />
       <div>{errors.PCN && <span className="error">{errors.PCN.message}</span>}</div>
-      <label><h5>Priest Email Address</h5></label><input className="create-note" type="email" name="PEA" ref={register({pattern: {value : /^\S+@\S+$/i , message:"Invalid email"}})} />
-      <div>{errors.PEA && <span className="error">{errors.PEA.message}</span>}</div>
-      <label><h5>Famous Deity</h5></label><input className="create-note" type="text"  name="FD" ref={register} />
-      <div>{errors.FD && <span className="error">{errors.FD.message}</span>}</div>
+      <label><h5>Priest Email Address</h5></label><input className="create-note" onChange={handleChange} type="email" name="PEA" ref={register({pattern: {value : /^\S+@\S+$/i , message:"Invalid email"}})} />
+      <div>{errors.PEA && <span className="error">{errors.PEA.message}</span>}</div> */}
+      <label><h5>Famous Deity</h5></label><input className="create-note" onChange={handleChange} value={temple.deity} type="text"  name="deity" ref={register} />
+      <div>{errors.deity && <span className="error">{errors.deity.message}</span>}</div>
       <input className="create-note-submit" type="submit" />
     </form>
     </div>
--- src/Temple.js
+++ src/Temple.js
@@ -1,5 +1,6 @@
 import React, {useState,useEffect} from 'react'
 import axios from 'axios';
+import {Redirect} from 'react-router-dom'
 import '@vaadin/vaadin-details';
 import '@vaadin/vaadin-accordion';
 import "@ui5/webcomponents/dist/TabContainer";
@@ -8,6 +9,7 @@ import "@ui5/webcomponents/dist/TabSeparator";
 import "@ui5/webcomponents/dist/Carousel.js";
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab'
+import "@ui5/webcomponents/dist/Button";
 
 function Temple({match}) {
 
@@ -18,6 +20,7 @@ function Temple({match}) {
     console.log(obj);
     const [temple, setTemple] = useState({});
     const [loading, setLoading] = useState(true);
+    const [deleted,setDeleted] = useState(false);
     useEffect(() => {
         axios.post("https://qzsnu26p30.execute-api.us-east-2.amazonaws.com/dev/temples/templeId",obj)
             .then(res => {
@@ -32,6 +35,23 @@ function Temple({match}) {
     // var temple = obj[0];
 
     // if(!temple) temple = data[0];
+    const payload = {
+        templeId : temple.templeId,
+        location : temple.location
+    };
+    
+    const handleDelete = () => {
+        axios.delete('https://qzsnu26p30.execute-api.us-east-2.amazonaws.com/dev/temples/remove', { data: payload })
+        .then(res => {
+            console.log(res);
+            setDeleted(true);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }
+
+    if(deleted) return <Redirect to='/' />
 
     return (
         <div>
@@ -49,6 +69,7 @@ function Temple({match}) {
                     <img src={temple.headerImageUrl} alt="new" height={350} width={500} />
                 </ui5-carousel>
                 </div>
+                <ui5-button onClick={handleDelete} design="Negative">Remove</ui5-button>    
             </Tab>
             <Tab eventKey="contact" title="Contact">
                 <div className="contact">
